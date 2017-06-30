2a47f036e7e5d5df3e2fbb1998a39670b0ba5c23
Alexander Omelchuk <cubesfriend@gmail.com>
2017-06-30 11:46:32 +0200

    Add project files.

---
+++ TestExceptionFilters/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace TestExceptionFilters
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        Console.WriteLine("Hello World!");
+      }
+      catch (Exception e) when (e is IOException)
+      {
+        Environment.ExitCode = 1;
+      }
+    }
+  }
+}
